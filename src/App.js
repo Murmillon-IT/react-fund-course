7783b3436f15c7847aba57f3f840e2992496e0a5
Murmillon-IT <a.azizkhanov@yandex.ru>
2023-09-28 03:37:42 +0500

    creating a counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
+
   return (
     <div className="App">
+      
+      <Counter/>
+      <ClassCounter/>
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { type } from '@testing-library/user-event/dist/type';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <App/>
 );
